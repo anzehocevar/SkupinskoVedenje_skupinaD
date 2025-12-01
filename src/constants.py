6dd02866d3539aef15121ce42be109b9137ee223
jan146 <anurjan@gmail.com>
2025-12-01 18:55:28 +0100

    Initial simulation loop

---
+++ src/constants.py
@@ -0,0 +1,47 @@
+# Number of fish/agents
+n_fish: int = 100
+
+# Length of attraction
+# Possible values: 1.5, 3, 5
+# Default: 3
+l_att: float = 3
+# Length of alignment
+# Possible values: 1.5, 3, 5
+# Default: 3
+l_ali: float = 3
+
+# Attraction and alignment strength/intensity
+# Possible values for gamma_att: [0.0, 0.6]
+# Possible values for gamma_ali: [0.0, 1.2]
+# k=1:
+#   Swarming: gamma_att, gamma_ali = (0.6, 0.6)
+#   Schooling: gamma_att, gamma_ali = (0.22, 0.6)
+#   Milling: gamma_att, gamma_ali = (0.37, 0.2)
+# k=2:
+#   Swarming: gamma_att, gamma_ali = (0.6, 0.2)
+#   Schooling: gamma_att, gamma_ali = (0.2, 0.3)
+gamma_att: float = 0.6
+gamma_ali: float = 0.6
+
+# Number of neighbours to consider before every kick
+# All 3 states are possible for k=1
+# Only schooling and swarming are possible for k=2
+k: int = 1
+
+# Noise intensity
+gamma_rand: float = 0.2
+
+# Kick duration and length
+tau_n_mean: float = 1.0
+tau_n_std: float = 1.0
+tau_n_min: float = 1e-3
+l_n_mean: float = 1.0
+
+# Relaxation time
+tau_0: float = 0.8
+
+# Coefficient of anisotropy
+eta: float = 0.8
+
+# Randomness seed
+seed: int = 123
---
+++ src/main.py
@@ -0,0 +1,79 @@
+import numpy as np
+import numpy.typing as npt
+import src.constants
+
+def initial_conditions(N: int) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
+    """Uniformly random placement of fish in circle with centre (0, 0) and radius R"""
+    R: float = (src.constants.l_att/2.0) * np.sqrt(N/np.pi)
+    r: npt.NDArray = R * np.sqrt(np.random.rand(N))
+    angle: npt.NDArray = np.random.rand(N) * 2 * np.pi
+    u_x: npt.NDArray = r * np.cos(angle)
+    u_y: npt.NDArray = r * np.sin(angle)
+    phi: npt.NDArray = np.random.rand(N) * 2 * np.pi
+    return u_x, u_y, phi
+
+def main():
+    np.random.seed(src.constants.seed)
+    u_x_last: npt.NDArray
+    u_y_last: npt.NDArray
+    phi: npt.NDArray
+    N: int = src.constants.n_fish
+    u_x_last, u_y_last, phi = initial_conditions(N)
+    # tau_min: npt.NDArray = np.repeat(src.constants.tau_n_min, N)
+    t_last: npt.NDArray = np.zeros(N)
+    tau: npt.NDArray = np.random.normal(loc=src.constants.tau_n_mean, scale=src.constants.tau_n_std, size=N)
+    # t_next: npt.NDArray = t_last + np.max((tau_min, tau), axis=0)
+    t_next: npt.NDArray = t_last + np.abs(tau)
+
+    while True:
+        # Find time and fish of next kick
+        i: int = int(np.argmin(t_next))
+        t: float = t_next[i]
+
+        # Compute time since all fish's last kicks
+        s: npt.NDArray = t - t_last
+
+        # Compute position of every fish at this time: u(t)
+        phi_unitvec_x, phi_unitvec_y = np.cos(phi[i]), np.sin(phi[i])
+        scale: npt.NDArray = tau * (
+            (1-np.exp(-s/src.constants.tau_0)) /
+            (1-np.exp(-tau/src.constants.tau_0))
+        )
+        u_x: npt.NDArray = u_x_last + scale * phi_unitvec_x
+        u_y: npt.NDArray = u_y_last + scale * phi_unitvec_y
+
+        # Compute distances from fish i to all other fish
+        u_x_i, u_y_i = u_x[i], u_y[i]
+        d: npt.NDArray = np.sqrt(np.square(u_x_i - u_x) + np.square(u_y_i - u_y))
+
+        # Compute angle(s) of perception for fish i
+        u_x_relative, u_y_relative = u_x - u_x_i, u_y - u_y_i
+        theta: npt.NDArray = np.arctan2(u_y_relative, u_x_relative)
+        psi: npt.NDArray = theta - phi
+
+        # Compute relative headings
+        phi_relative: npt.NDArray = phi - phi[i]
+
+        # Compute the heading angle changes
+        d_sq: npt.NDArray = np.square(d)
+        delta_phi: npt.NDArray = (
+            src.constants.gamma_att * ((d * np.sin(psi)) / (1+d_sq/np.square(src.constants.l_att))) +
+            src.constants.gamma_ali * (1 + src.constants.eta * np.cos(psi)) * np.exp(-d_sq/np.square(src.constants.l_ali)) * np.sin(phi_relative)
+        )
+        influence: npt.NDArray = np.abs(delta_phi)
+        top_k_indexes: npt.NDArray = np.argpartition(influence, -src.constants.k)[-src.constants.k:]
+
+        # Compute new heading
+        phi[i] = phi[i] + src.constants.gamma_rand * np.random.normal(loc=0, scale=1) + np.sum(delta_phi[top_k_indexes])
+
+        # Prepare for next kick
+        tau_i: float = np.random.normal(loc=src.constants.tau_n_mean, scale=src.constants.tau_n_std)
+        l_i: float = tau_i
+        tau[i] = tau_i
+        t_last[i] = t
+        t_next[i] = t + tau_i
+        u_x_last[i] = u_x_last[i] + phi_unitvec_x * l_i
+        u_y_last[i] = u_y_last[i] + phi_unitvec_y * l_i
+
+if __name__ == "__main__":
+    main()
