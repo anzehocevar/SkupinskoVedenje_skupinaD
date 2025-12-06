5bc957e65edcf39db4306c1c73b9948b99a4eb9d
Maks Popović <101988551+mpopovic4116@users.noreply.github.com>
2025-12-06 18:02:20 +0100

    More accurate progress indicator

--- src/cb25d/compare_gamma_original.py
+++ src/cb25d/compare_gamma_original.py
@@ -1,13 +1,11 @@
 # pyright: strict
 from collections.abc import Callable
-from itertools import product
-from typing import cast
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from functools import cache
 
 import cloudpickle  # pyright: ignore[reportMissingTypeStubs]
 import numpy as np
-from tqdm.contrib.concurrent import (
-    process_map,  # pyright: ignore[reportUnknownVariableType]
-)
+from tqdm.auto import tqdm
 
 from cb25d.batch import run_batch_simulation
 from cb25d.simulation_impl_original import (
@@ -25,15 +23,17 @@ type CreateInitialState = Callable[
 ]
 
 
+@cache
+def _uncloudpickle(create_initial_state_b: bytes) -> CreateInitialState:
+    return cloudpickle.loads(create_initial_state_b)
+
+
 def _run_single_simulation(
     value: tuple[int, float, float, bytes, int],
 ) -> tuple[float, float, float]:
     seed, att, ali, create_initial_state_b, steps_per_run = value
-    create_initial_state = cast(
-        CreateInitialState, cloudpickle.loads(create_initial_state_b)
-    )
     run_batch_simulation(
-        create_initial_state(att, ali, seed),
+        _uncloudpickle(create_initial_state_b)(att, ali, seed),
         rec := SimulationRecorderOriginal(skip_first_n=steps_per_run // 2),
         steps=steps_per_run,
     )
@@ -49,29 +49,25 @@ def run_gamma_comparison_original(
     runs_per_config: int,
     steps_per_run: int,
 ):
-    n_runs = len(att_vals) * len(ali_vals) * runs_per_config
-    seed *= n_runs
+    seed *= len(att_vals) * len(ali_vals) * runs_per_config
     statistics = np.zeros((len(att_vals), len(ali_vals), 3))
     create_initial_state_b = cloudpickle.dumps(create_initial_state)  # pyright: ignore[reportUnknownMemberType]
-    for stats, (i, j, _) in zip(
-        cast(
-            list[tuple[float, float, float]],
-            process_map(
-                _run_single_simulation,
-                [
-                    (seed + i, *args)
-                    for i, args in enumerate(
-                        (att, ali, create_initial_state_b, steps_per_run)
-                        for att in att_vals
-                        for ali in ali_vals
-                        for _ in range(runs_per_config)
-                    )
-                ],
-                chunksize=10,
-            ),
-        ),
-        product(range(len(att_vals)), range(len(ali_vals)), range(runs_per_config)),
-    ):
-        statistics[i, j, :] += stats
+
+    futures = {
+        (seed + i, *args): ij
+        for i, (ij, args) in enumerate(
+            ((i_att, i_ali), (att, ali, create_initial_state_b, steps_per_run))
+            for i_att, att in enumerate(att_vals)
+            for i_ali, ali in enumerate(ali_vals)
+            for _ in range(runs_per_config)
+        )
+    }
+
+    with ProcessPoolExecutor() as ppx, tqdm(total=len(futures)) as progress:
+        futures = {ppx.submit(_run_single_simulation, k): v for k, v in futures.items()}
+        for future in as_completed(futures):
+            i, j = futures[future]
+            statistics[i, j, :] += future.result()
+            progress.update()
     statistics /= runs_per_config
     return statistics
