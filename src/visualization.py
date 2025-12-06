bd19097a391ee4bc7815f2c9e3c4aa7444248438
jan146 <anurjan@gmail.com>
2025-12-06 17:37:06 +0100

    Visualize groups using colors

--- src/visualization.py
+++ src/visualization.py
@@ -14,12 +14,37 @@ LENGTH_DIRECTION: float = 5.0
 WIDTH_DIRECTION: int = 1
 FPS: int = 60
 
+def generate_colorspace() -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
+    red: npt.NDArray = np.zeros(6*255)
+    green: npt.NDArray = np.zeros(6*255)
+    blue: npt.NDArray = np.zeros(6*255)
+    offset: int = 0
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = 0, 255, i
+    offset += 255
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = 0, 255-i, 255
+    offset += 255
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = i, 0, 255
+    offset += 255
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = 255, 0, 255-i
+    offset += 255
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = 255, i, 0
+    offset += 255
+    for i in range(255):
+        red[i+offset], green[i+offset], blue[i+offset] = 255-i, 255, 0
+    return red, green, blue
+
 def main():
 
     # Initialisation
     pygame.init()
     display: pygame.Surface = pygame.display.set_mode(DISPLAY_SIZE)
     clock: pygame.time.Clock = pygame.time.Clock()
+    red, green, blue = generate_colorspace()
     simulation: Iterator[tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]] = src.simulation.run_with_groups()
     running: bool = True
     iter: int = 0
@@ -46,13 +71,18 @@ def main():
 
         # Visualize state every ITERATIONS_BETWEEN_VISUAL_UPDATES kicks
         if iter % ITERATIONS_BETWEEN_VISUAL_UPDATES == 0:
+            groups: npt.NDArray = np.unique(group)
+            index_colorspace: npt.NDArray = np.linspace(0, 6*255, len(groups), endpoint=False).astype(int)
+            group_to_index: npt.NDArray = np.full(groups.max()+1, -1)
+            group_to_index[groups] = np.arange(len(groups))
             display.fill((0, 0, 0))
-            for x, y, dx, dy in zip(u_x, u_y, np.cos(phi), np.sin(phi)):
+            for x, y, dx, dy, ix in zip(u_x, u_y, np.cos(phi), np.sin(phi), group_to_index[group]):
                 offset_x, offset_y = DISPLAY_SIZE[0]//2, DISPLAY_SIZE[1]//2
                 dx, dy = LENGTH_DIRECTION*dx, LENGTH_DIRECTION*dy
                 x, y = x+offset_x, y+offset_y
-                pygame.draw.circle(display, COLOR_POSITION, (x, y), SIZE_POSITION)
-                pygame.draw.line(display, COLOR_DIRECTION, (x, y), (x+dx, y+dy), WIDTH_DIRECTION)
+                color: tuple[int, int, int] = (red[index_colorspace[ix]], green[index_colorspace[ix]], blue[index_colorspace[ix]])
+                pygame.draw.circle(display, color, (x, y), SIZE_POSITION)
+                pygame.draw.line(display, color, (x, y), (x+dx, y+dy), WIDTH_DIRECTION)
             pygame.display.flip()
             clock.tick(FPS)
 
